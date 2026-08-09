90a2d479cda54ecb3e10cda91833251fa533caad
julialack <julia.lackchristensen@gmail.com>
2026-08-09 10:02:53 +0200

    Fixad Stripe API version för lyckad build på Vercel

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 import { createClient } from '@/utils/supabase/server';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2026-07-29.dahlia',
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
