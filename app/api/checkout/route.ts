60b3a7dc191f09c562cd21d24f5f4e2cd49c0425
julialack <julia.lackchristensen@gmail.com>
2026-08-09 10:42:39 +0200

    Inaktiverat Swish tillfälligt för att åtgärda fel i Stripe Checkout

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
     }
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card', 'swish'],
+      payment_method_types: ['card'],
       line_items: [
         {
           price_data: {
